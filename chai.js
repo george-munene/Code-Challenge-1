e8e0213d53129388e53b56d54c16a3adc92cc458
george-munene <ngangaredeemed@gmail.com>
2025-06-10 17:25:41 +0300

    Updates comments

--- chai.js
+++ chai.js
@@ -1,16 +1,18 @@
-function calculateChaiIngredients() {
-    const numberOfCups = parseInt(prompt("Karibu! How many cups of Chai Bora would you like to make?"), 10);
+function calculateChaiIngredients() {  //creates the function
+    const numberOfCups = parseInt(prompt("Karibu! How many cups of Chai Bora would you like to make?"), 10); //prompts user for input, saves it, and defaults to 10
 
-    if (isNaN(numberOfCups) || numberOfCups <= 0) {
-        console.log("Please enter a valid number of cups.");
+    if (isNaN(numberOfCups) || numberOfCups <= 0) { //checks if a valid number is input
+        console.log("Please enter a valid number of cups."); //displays to the user the error message
         return;
     }
 
+    //defining variables for ingredients, makes calculation and saves into the variables
     const water = numberOfCups * 200; 
     const milk = numberOfCups * 50;
     const teaLeaves = numberOfCups;
     const sugar = numberOfCups * 2; 
 
+    //displays the output to the user
     console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
     console.log(`Water: ${water} ml`);
     console.log(`Milk: ${milk} ml`);
