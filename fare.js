3cef249ff6be02a27de263b820f36adf8da6744b
george-munene <ngangaredeemed@gmail.com>
2025-06-10 17:27:45 +0300

    Adds critical comments

--- fare.js
+++ fare.js
@@ -1,16 +1,19 @@
-function calculateBodaFare() {
-    const distanceInKm = parseFloat(prompt("Unafika wapi Mkubwa? Kilometer ngapi?:"));
+function calculateBodaFare() { //creates the function to calculate fare
+    const distanceInKm = parseFloat(prompt("Unafika wapi Mkubwa? Kilometer ngapi?:")); //accepts user input
 
+    //checks if number is entered
     if (isNaN(distanceInKm) || distanceInKm < 0) {
         console.log("Please enter a valid distance.");
         return;
     }
 
+    //defines variables, does calculations and saves into the variables
     const baseFare = 50; 
     const chargePerKm = 15;
     const chargebyKM = (distanceInKm * chargePerKm)
     const totalFare = baseFare + chargebyKM
 
+    //diplays output to the user
     console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
     console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
     console.log(`Mpaka Uko: KES ${chargebyKM}`);
@@ -18,5 +21,5 @@ function calculateBodaFare() {
     console.log("Panda Pikipiki!");
 }
 
-
+//runs the funtion
 calculateBodaFare();
