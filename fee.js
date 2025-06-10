a62f4455f6787ba98308839ea1475a9d6423d8db
george-munene <ngangaredeemed@gmail.com>
2025-06-10 17:31:35 +0300

    updates the comments for fee.js

--- fee.js
+++ fee.js
@@ -1,28 +1,35 @@
+// defines function to estimate Transaction Fee
 function estimateTransactionFee() {
+    //accepts input and converts input into a number
     const amountToSend = parseFloat(prompt("Unatuma Ngapi? (KES):"));
 
+    //rejects the input if not a valid input
     if (isNaN(amountToSend) || amountToSend <= 0) {
         console.log("Please enter a valid amount.");
         return;
     }
 
+    //calculates transaction fee
     const feePercentage = 0.015; // 1.5%
     let transactionFee = amountToSend * feePercentage;
 
-    
+
+    // adds conditional transaction fee
     if (transactionFee < 10) {
         transactionFee = 10;
     } else if (transactionFee > 70) {
         transactionFee = 70;
     }
 
+    //calculates total transaction fee
     const totalDebited = amountToSend + transactionFee;
 
+    //displays output to user
     console.log(`Sending KES ${amountToSend}:`);
     console.log(`Calculated Transaction Fee: KES ${transactionFee}`);
     console.log(`Total amount to be debited: KES ${totalDebited}`);
     console.log("Send Money Securely!");
 }
 
-
+//runs the function
 estimateTransactionFee();
